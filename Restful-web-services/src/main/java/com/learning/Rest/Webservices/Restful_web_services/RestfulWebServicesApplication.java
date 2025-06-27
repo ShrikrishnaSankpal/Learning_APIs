32e710b8716f549a213541dfa7f9faf4ee0fab70
ShrikrishnaSankpal <87565566+ShrikrishnaSankpal@users.noreply.github.com>
2025-06-27 09:30:58 +0530

    Restful-Web-services

---
+++ Restful-web-services/src/main/java/com/learning/Rest/Webservices/Restful_web_services/RestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Rest.Webservices.Restful_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+}
