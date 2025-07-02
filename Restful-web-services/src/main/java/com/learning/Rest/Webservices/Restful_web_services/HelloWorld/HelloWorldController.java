a58b82376aff567506885109d32b8843b49a3b7b
ShrikrishnaSankpal <87565566+ShrikrishnaSankpal@users.noreply.github.com>
2025-07-02 09:02:10 +0530

    Hello World Rest API

---
+++ Restful-web-services/src/main/java/com/learning/Rest/Webservices/Restful_web_services/HelloWorld/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.learning.Rest.Webservices.Restful_web_services.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(method= RequestMethod.GET, path ="/Hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	
+	// Another Way of Writing above API 
+	
+	@GetMapping("/HelloWorld")
+	public String hello_World() {
+		return "Hello World with GetMapping";
+	}
+	
+	
+	//Hello World API with Java Bean
+	
+	@GetMapping("/HelloWorldBean")
+	public HelloWorldBean hello_WorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
+}
