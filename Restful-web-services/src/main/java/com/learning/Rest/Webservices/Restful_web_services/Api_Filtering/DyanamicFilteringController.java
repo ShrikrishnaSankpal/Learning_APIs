026b9e884406b754652a96d7ae77330434ff9d9a
ShrikrishnaSankpal <87565566+ShrikrishnaSankpal@users.noreply.github.com>
2025-08-24 11:09:59 +0530

    dyanmic filtering with list

--- Restful-web-services/src/main/java/com/learning/Rest/Webservices/Restful_web_services/Api_Filtering/DyanamicFilteringController.java
+++ Restful-web-services/src/main/java/com/learning/Rest/Webservices/Restful_web_services/Api_Filtering/DyanamicFilteringController.java
@@ -1,5 +1,8 @@
 package com.learning.Rest.Webservices.Restful_web_services.Api_Filtering;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +18,33 @@ public class DyanamicFilteringController {
 	public MappingJacksonValue filtering() {
 
 		DyanmicFilterPmpBean empBean = new DyanmicFilterPmpBean("value1", "value2", "value3");
+		
 		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(empBean);
+		
 		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		
 		FilterProvider filters = new SimpleFilterProvider().addFilter("EmpBeanFilter", filter);
+		
 		mappingJacksonValue.setFilters(filters);
+		
+		return mappingJacksonValue;
+	}
+	
+	
+	@GetMapping("/api/DynamicFilteringWithList")
+	public MappingJacksonValue filteringList() {
+		
+		List<DyanmicFilterPmpBean> List = Arrays.asList(new DyanmicFilterPmpBean("value1", "value2", "value3"), 
+				new DyanmicFilterPmpBean("value1", "value2", "value3"));
+		
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(List);
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("EmpBeanFilter", filter);
+		
+		mappingJacksonValue.setFilters(filters);
+		
 		return mappingJacksonValue;
 	}
 
